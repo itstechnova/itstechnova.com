c344510025068ec9de8cdaaa7382e02c3472022c
Amy Shao <amyshao15@gmail.com>
2021-05-17 02:17:47 -0700

    18: use react-router-hash-link to create links to section (#10)
    Co-authored-by: Amy Shao <amys@ensemble.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,31 @@
 import "./App.scss";
 import Landing from "./sections/Landing/Landing";
-//import NavBar from "./sections/NavBar/NavBar";
-//import About from "./sections/About/About";
-//import nav from "./resources/strings/nav";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NavBar from "./sections/NavBar/NavBar";
+import About from "./sections/About/About";
+import nav from "./resources/strings/nav";
+import { BrowserRouter } from 'react-router-dom';
 
 /*
 const routes = [
     { label: nav.title, sectionId: "landing" },
     { label: nav.about, sectionId: "about" },
 ];
-*/
+
 
 // PDF locations in public folder
 //const SponsorPackage = `${process.env.PUBLIC_URL}/pdfs/sponsorship_package.pdf`;
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                {/*<Route
-                    // Redirect itstechnova.org/sponsorship-package to pdf location
-                    path="/sponsors"
-                    component={() => {
-                        window.location.href = SponsorPackage;
-                        return null;
-                    }}
-                />*/}
-                <Route exact path="/">
-                    <div className="App">
-                        {/*<NavBar routes={routes} />*/}
-                        <Landing />
-                        {/*<About />*/}
-                    </div>
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <NavBar routes={routes} />
+        <Landing />
+        <About />
+      </div>
+    </BrowserRouter>
+
+  );
 };
 
 export default App;
--- src/sections/NavBar/NavBar.jsx
+++ src/sections/NavBar/NavBar.jsx
@@ -4,25 +4,25 @@ import "./NavBar.scss";
 import nav from "../../resources/strings/nav";
 
 const NavBar = ({ routes }) => {
-    return (
-        <nav>
-            <div className="nav-container">
-                {routes.map((route) => (
-                    <Link
-                        className="nav-link"
-                        activeClass="active"
-                        to={`/#${route.sectionId}`}
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >
-                        {route.label}
-                    </Link>
-                ))}
-            </div>
-        </nav>
-    );
+  return (
+    <nav>
+      <div className="nav-container">
+        {routes.map((route) => (
+          <Link
+            className="nav-link"
+            activeClass="active"
+            to={`/#${route.sectionId}`}
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            {route.label}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
