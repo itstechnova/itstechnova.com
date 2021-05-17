54f9bb5c45edcffe3a8c3b86805e8fb820ab5176
Amy Shao <amyshao15@gmail.com>
2021-05-17 02:32:56 -0700

    Feature/32 sponsor package page (#11)
    * convert to 4 spaces for tabs
    
    * 32: add dummy sponsor package pdf page
    
    * 32: add _redirects file for react router to work with netlify
    
    Co-authored-by: Amy Shao <amys@ensemble.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,38 @@ import Landing from "./sections/Landing/Landing";
 import NavBar from "./sections/NavBar/NavBar";
 import About from "./sections/About/About";
 import nav from "./resources/strings/nav";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const routes = [
-  { label: nav.title, sectionId: "landing" },
-  { label: nav.about, sectionId: "about" },
+    { label: nav.title, sectionId: "landing" },
+    { label: nav.about, sectionId: "about" },
 ];
 
+// PDF locations in public folder
+const SponsorPackage = `${process.env.PUBLIC_URL}/pdfs/sponsorship_package.pdf`;
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <NavBar routes={routes} />
-        <Landing />
-        <About />
-      </div>
-    </BrowserRouter>
-
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route
+                    // Redirect itstechnova.org/sponsorship-package to pdf location
+                    path="/sponsorship-package"
+                    component={() => {
+                        window.location.href = SponsorPackage;
+                        return null;
+                    }}
+                />
+                <Route exact path="/">
+                    <div className="App">
+                        <NavBar routes={routes} />
+                        <Landing />
+                        <About />
+                    </div>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 };
 
 export default App;
--- src/resources/strings/landing.js
+++ src/resources/strings/landing.js
@@ -1,17 +1,17 @@
 const landing = {
-  title: "TechNova",
-  sloganPrepare: "Prepare with Us",
-  sloganHack: "Hack with Us",
-  sloganConnect: "Connect with Us",
-  description1: "University of Waterloo's First",
-  description2: "Virtual ",
-  descriptionBolded: "Women in Technology",
-  description3: " Hackathon",
-  comingSoon: "Coming Summer 2021",
-  mailingListCTA: "Join the mailing list",
-  sponsor: "Interested in Sponsoring?",
-  emailUs: "Email us at ",
-  email: "hacktechnova@gmail.com"
+    title: "TechNova",
+    sloganPrepare: "Prepare with Us",
+    sloganHack: "Hack with Us",
+    sloganConnect: "Connect with Us",
+    description1: "University of Waterloo's First",
+    description2: "Virtual ",
+    descriptionBolded: "Women in Technology",
+    description3: " Hackathon",
+    comingSoon: "Coming Summer 2021",
+    mailingListCTA: "Join the mailing list",
+    sponsor: "Interested in Sponsoring?",
+    emailUs: "Email us at ",
+    email: "hacktechnova@gmail.com"
 };
 
 export default landing;
