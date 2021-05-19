d0a8668ea1465da4268e2855dfe04e6d824e5325
Amy Shao <amyshao15@gmail.com>
2021-05-18 18:57:53 -0700

    21: added all pages and section containers

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => (
+    <React.Fragment id="footer">
+        <div className="Footer-container">
+            <span className="title">Footer</span>
+        </div>
+    </React.Fragment>
+);
+
+export default Footer;
---
+++ src/sections/Story/Story.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
+import "./Story.scss";
+
+const Story = () => (
+    <SectionWrapper id="story">
+        <div className="story-container">
+            <span className="title">Our Story</span>
+        </div>
+    </SectionWrapper>
+);
+
+export default Story;
