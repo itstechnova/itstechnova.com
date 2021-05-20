1fe27caf9f8a17e8ae3d62e6570c66eec3e2f2c2
Amy Shao <amyshao15@gmail.com>
2021-05-20 03:32:48 -0700

    21: nav bar disappears on scroll

--- src/sections/NavBar/NavBar.jsx
+++ src/sections/NavBar/NavBar.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import { HashLink as Link } from "react-router-hash-link";
+import { debounce } from '../../util/debounce';  
 import "./NavBar.scss";
 import ModeToggle from "../../components/ModeToggle/ModeToggle";
 
-const NavBar = ({ routes, logoRoute }) => {
+const NavBar = ({routes, logoRoute}) => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0); 
+  const [visible, setVisible] = useState(true);  
+
+  const handleScroll = debounce(() => {
+    const currentScrollPos = window.pageYOffset;
+
+    setVisible((prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 70) || currentScrollPos < 10);
+
+    setPrevScrollPos(currentScrollPos);
+  }, 100);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+
+  }, [prevScrollPos, visible, handleScroll]);
+
     return (
         <nav>
-            <div className="nav-container">
+            <div className="nav-container"  style={{top: visible ? '0' : '-7vh'}} >
                 <div className="nav-links">
                     <Link
                         className="logo-link"
