4a64e9dcdc70fe4f57110deeb0fbb1714bb2fce5
Amy Shao <amyshao15@gmail.com>
2021-05-20 02:22:20 -0700

    21: updated nav bar

--- src/sections/NavBar/NavBar.jsx
+++ src/sections/NavBar/NavBar.jsx
@@ -1,27 +1,40 @@
 import React from "react";
-import { HashLink as Link } from 'react-router-hash-link';
+import { HashLink as Link } from "react-router-hash-link";
 import "./NavBar.scss";
-import nav from "../../resources/strings/nav";
 import ModeToggle from "../../components/ModeToggle/ModeToggle";
 
-const NavBar = ({ routes }) => {
-
+const NavBar = ({ routes, logoRoute }) => {
     return (
         <nav>
             <div className="nav-container">
-                {routes.map((route) => (
+                <div className="nav-links">
                     <Link
-                        className="nav-link"
+                        className="logo-link"
                         activeClass="active"
-                        to={`/#${route.sectionId}`}
+                        to={`/#${logoRoute.sectionId}`}
                         spy={true}
                         smooth={true}
                         offset={-70}
                         duration={500}
                     >
-                        {route.label}
+                        <img src={logoRoute.logoSrc} alt="logo" />
                     </Link>
-                ))}
+                    <div>
+                        {routes.map((route) => (
+                            <Link
+                                className="nav-link"
+                                activeClass="active"
+                                to={`/#${route.sectionId}`}
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                            >
+                                {route.label}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
                 <ModeToggle />
             </div>
         </nav>
