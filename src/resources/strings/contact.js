f6facc3126fc2616a3b65ed4b45b97dc042e5d5c
Jasmine Bal <baljasminek@gmail.com>
2021-05-24 19:44:36 -0400

    43: cleaned up code

---
+++ src/resources/strings/contact.js
@@ -0,0 +1,13 @@
+const contact = {
+    title: "Contact Us",
+    link1: "Open Source",
+    href1: "",
+    link2: "Privacy Policy",
+    href2: "",
+    link3: "Code of Conduct",
+    href3: "",
+    link4: "WiCS",
+    href4: "",
+};
+
+export default contact;
--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -9,11 +9,7 @@ import ig from "../../resources/images/socials/igFill.svg";
 import linkedin from "../../resources/images/socials/linkedinFill.svg";
 import facebook from "../../resources/images/socials/facebookFill.svg";
 import twitter from "../../resources/images/socials/twitterFill.svg";
-
-///For reference:
-/// URL: https://docs.google.com/forms/u/0/d/e/1FAIpQLSdUCqXqrW53nkw3xM5MDDjreqfnCNYGuIMcZZe_EvKZItrgiw/formResponse
-/// Field ID: entry.1170762674
-
+import contact from "../../resources/strings/contact";
 
 function Contact () {
     
@@ -26,8 +22,7 @@ function Contact () {
         const bodyValues = {
             'entry.1170762674': state.email,
           }
-//application/x-www-form-urlencoded
-        //var content = new FormUrlEncodedContent(bodyValues);
+
         const response =  fetch(url, {
             "method": "POST",
             "headers": {
@@ -35,17 +30,14 @@ function Contact () {
                 'access-control-allow-origin': '*', 
               },
             "body": `entry.1170762674=${state.email}`})
-        
       }
-
-
 
     return (
     <SectionWrapper id="contact">
         <div className="contact-container">
             <div className="contact-row">
                 <div className="contact">
-                    <h2 className="sub-title">Contact Us</h2>
+                    <h2 className="sub-title">{contact.title}</h2>
                     <div className="email-wrapper">
                     
                         <input className="email-input" onChange={e=> setState({...state, email: e.target.value})}/>
@@ -66,10 +58,10 @@ function Contact () {
                     </div>
 
                     <div className="links-row">
-                        <a className="contact-links" href="google.com">Open Source</a>
-                        <a className="contact-links"href="google.com">Privacy Policy</a>
-                        <a className="contact-links" href="google.com">Code of Conduct</a>
-                        <a className="contact-links" href="google.com">WiCS</a>
+                        <a className="contact-links" href={contact.href1}>{contact.link1}</a>
+                        <a className="contact-links"href={contact.href2}>{contact.link2}</a>
+                        <a className="contact-links" href={contact.href3}>{contact.link3}</a>
+                        <a className="contact-links" href={contact.href4}>{contact.link4}</a>
                     </div>
                    
                 </div>
