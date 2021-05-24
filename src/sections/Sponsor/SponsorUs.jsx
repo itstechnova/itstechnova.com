acae3ce7bc298ca2fed7632b35ff12bd48779745
Grace Nguyen <42276283+gracenng@users.noreply.github.com>
2021-05-24 13:42:06 -0400

    All but carousel

--- src/sections/Sponsor/SponsorUs.jsx
+++ src/sections/Sponsor/SponsorUs.jsx
@@ -15,49 +15,8 @@ const SponsorUs = () => (
                 </p>
             </div>
             <div className="sponsor-us-image"> </div> {/*TBD by design*/}
-
             <button className="sponsor-us-button">Join as a Sponsor</button>
-
             <div className="sponsor-us-showcase">Sponsor Showcase</div>
-            
-            <div className="sponsor-us-caroussel">
-                <Carousel>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src="../../sponsors/facebook.png"
-                        alt="First slide"
-                        />
-                        <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src="../../sponsors/facebook.png"
-                        alt="Second slide"
-                        />
-
-                        <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src="../../sponsors/facebook.png"
-                        alt="Third slide"
-                        />
-                        <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-            </div>
 
         </div>
     </SectionWrapper>
