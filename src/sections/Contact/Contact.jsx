e1f2f3ba57bef160d8983000cb3e33ab380e51ad
Amy Shao <amyshao15@gmail.com>
2021-05-25 02:57:41 -0400

    43: fix build errors

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {useState} from 'react';
+import { useState } from 'react';
 import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
 import "./Contact.scss";
 import arrow from "../../resources/images/icons/submit.svg";
@@ -11,69 +11,73 @@ import facebook from "../../resources/images/socials/facebookFill.svg";
 import twitter from "../../resources/images/socials/twitterFill.svg";
 import contact from "../../resources/strings/contact";
 
-function Contact () {
-    
+function Contact() {
+
     const [state, setState] = useState({
         email: null
     })
 
-    async function submitEmail () {
+    async function submitEmail() {
+        /*
         const url = `https://docs.google.com/forms/u/0/d/e/1FAIpQLSdUCqXqrW53nkw3xM5MDDjreqfnCNYGuIMcZZe_EvKZItrgiw/formResponse`
+        
         const bodyValues = {
             'entry.1170762674': state.email,
-          }
+        }
 
-        const response =  fetch(url, {
+        const response = fetch(url, {
             "method": "POST",
             "headers": {
                 "content-type": "application/x-www-form-urlencoded",
-                'access-control-allow-origin': '*', 
-              },
-            "body": `entry.1170762674=${state.email}`})
-      }
+                'access-control-allow-origin': '*',
+            },
+            "body": `entry.1170762674=${state.email}`
+        })
+        */
+    }
 
     return (
-    <SectionWrapper id="contact">
-        <div className="contact-container">
-            <div className="contact-row">
-                <div className="contact">
-                    <h2 className="sub-title">{contact.title}</h2>
-                    <div className="email-wrapper">
-                    
-                        <input className="email-input" onChange={e=> setState({...state, email: e.target.value})}/>
-                        <div className="submit-wrapper" onClick={e => submitEmail()}>
-                            <div className="wrapper">
-                                <img src={arrow} className="submit-btn"/>   
+        <SectionWrapper id="contact">
+            <div className="contact-container">
+                <div className="contact-row">
+                    <div className="contact">
+                        <h2 className="sub-title">{contact.title}</h2>
+                        <div className="email-wrapper">
+
+                            <input className="email-input" onChange={e => setState({ ...state, email: e.target.value })} />
+                            <div className="submit-wrapper" onClick={e => submitEmail()}>
+                                <div className="wrapper">
+                                    <img src={arrow} className="submit-btn" alt="email submit"/>
+                                </div>
                             </div>
+
                         </div>
-                    
-                    </div>
 
-                    <div className="socials-row">
-                        <img src={mail}/>
-                        <img src={ig}/>
-                        <img src={linkedin}/>
-                        <img src={facebook}/>
-                        <img src={twitter}/>
-                    </div>
+                        <div className="socials-row">
+                            <img src={mail} alt="mail" />
+                            <img src={ig} alt="ig" />
+                            <img src={linkedin} alt="linkedin" />
+                            <img src={facebook} alt="facebook" />
+                            <img src={twitter} alt="twitter" />
+                        </div>
 
-                    <div className="links-row">
-                        <a className="contact-links" href={contact.href1}>{contact.link1}</a>
-                        <a className="contact-links"href={contact.href2}>{contact.link2}</a>
-                        <a className="contact-links" href={contact.href3}>{contact.link3}</a>
-                        <a className="contact-links" href={contact.href4}>{contact.link4}</a>
+                        <div className="links-row">
+                            <a className="contact-links" href={contact.href1}>{contact.link1}</a>
+                            <a className="contact-links" href={contact.href2}>{contact.link2}</a>
+                            <a className="contact-links" href={contact.href3}>{contact.link3}</a>
+                            <a className="contact-links" href={contact.href4}>{contact.link4}</a>
+                        </div>
+
                     </div>
-                   
+                    <img classname="contact-image" src={contactImage} alt="contact" />
                 </div>
-                <img classname="contact-image" src={contactImage}/>
-            </div>
-            
-            <h2 className="team-title">Meet the Team</h2>
 
-            
-        </div>
-    </SectionWrapper>
+                <h2 className="team-title">Meet the Team</h2>
+
+
+            </div>
+        </SectionWrapper>
     );
-    
+
 }
 export default Contact;
