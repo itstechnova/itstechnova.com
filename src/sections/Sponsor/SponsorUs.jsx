6cc1f931a7b87bfa7a81e3dc04f65e2da038a855
Grace Nguyen <42276283+gracenng@users.noreply.github.com>
2021-05-25 02:53:24 -0400

    Feature/26 sponsor us (#19)
    * Initial Commit
    
    * All but carousel
    
    * 26: Fix build errors
    
    Co-authored-by: Amy Shao <amyshao15@gmail.com>

--- src/sections/Sponsor/SponsorUs.jsx
+++ src/sections/Sponsor/SponsorUs.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+//import Carousel from 'react-bootstrap/Carousel';
 import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
 import "./SponsorUs.scss";
 
 const SponsorUs = () => (
     <SectionWrapper id="sponsor-us">
         <div className="sponsor-us-container">
-            <span className="title">Sponsor Us</span>
+            <h2 className="sponsor-us-title">Sponsor Us</h2>
+            <div className="sponsor-us-description">
+                <p>Support more than 200 young women in the next steps
+                of their journey by sponsoring the next generation of leaders</p>
+                <p>Interested in sponsoring? Email us at {'\u00A0'}
+                    <a href="mailto:sponsors@itstechnova.org">sponsors@itstechnova.org</a>
+                </p>
+            </div>
+            <div className="sponsor-us-image"> </div> {/*TBD by design*/}
+            <button className="sponsor-us-button">Join as a Sponsor</button>
+            <div className="sponsor-us-showcase">Sponsor Showcase</div>
+
         </div>
     </SectionWrapper>
 );
