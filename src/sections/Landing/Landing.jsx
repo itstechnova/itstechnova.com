f10865361a633bf67229d6a0449298d18438e81e
Amy Shao <amyshao15@gmail.com>
2021-05-25 04:28:10 -0400

    23: added text loop

--- src/sections/Landing/Landing.jsx
+++ src/sections/Landing/Landing.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Typist from 'react-typist';
 import "./Landing.scss";
 import socials from "../../resources/strings/socials";
 import landing from "../../resources/strings/landing";
 import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
 
+const TextLoop = () => {
+    const [mounted, setMounted] = useState(true);
+
+    useEffect(() => {
+        console.log("Count: " + mounted);
+        setMounted(true);
+    }, [mounted]);
+
+    return (
+        <div>
+            {mounted ? (
+                <Typist className="landing-slogan" onTypingDone={() => setMounted(false)}>
+                    <span><span style={{ fontWeight: "700" }}>Prepare</span> With Us</span>
+                    <Typist.Backspace count={20} delay={800} />
+                    <span><span style={{ fontWeight: "700" }}>Hack</span> With Us</span>
+                    <Typist.Backspace count={17} delay={800} />
+                    <span><span style={{ fontWeight: "700" }}>Connect</span> With Us</span>
+                    <Typist.Backspace count={20} delay={800} />
+                </Typist>
+            ) : (
+                <span className="landing-slogan">
+                    {"n"}
+                </span>
+            )}
+        </div>
+    );
+
+}
+
 const Landing = () => {
-
-
     return (
         <SectionWrapper id="landing">
             <div className="landing-container">
@@ -17,14 +44,7 @@ const Landing = () => {
                             <span className="landing-title">{landing.title1}</span>
                             {landing.title2}
                         </h4>
-                        <Typist className="landing-slogan">
-                            <span><span style={{ fontWeight: "700" }}>Prepare</span> With Us</span>
-                            <Typist.Backspace count={15} delay={500} />
-                            <span><span style={{ fontWeight: "700" }}>Hack</span> With Us</span>
-                            <Typist.Backspace count={12} delay={500} />
-                            <span><span style={{ fontWeight: "700" }}>Connect</span> With Us</span>
-                            <Typist.Backspace count={15} delay={500} />
-                        </Typist>
+                        <TextLoop />
                         <h4>{landing.description}</h4>
                         <h4>{landing.dates}</h4>
                         <a className="button" href={socials.mailingList}>
