67f67e32bb0a0c9bcea3cc62edbe03282883540c
Amy Shao <amyshao15@gmail.com>
2021-05-25 21:59:02 -0400

    Feature/24 about page (#23)
    * 24: info and cta
    
    * 24: about page wip

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
 import "./About.scss";
+import about from "../../resources/strings/about";
+import logo from "../../resources/images/logo/logo.png";
 
 const About = () => (
     <SectionWrapper id="about" >
         <div className="about-container">
-            <h1>header 1</h1>
-            <p> paragraph text</p>
-            <h2>header 2</h2>
-            <p> paragraph text</p>
-            <h3>header 3</h3>
-            <p> paragraph text</p>
-            <h4>header 4</h4>
-            <p> paragraph text</p>
-            <h5>header 5</h5>
-            <p> paragraph text</p>
+            <div className="about-content">
+                <div className="about-text">
+                    <h1>
+                        {about.title}
+                    </h1>
+                    <br />
+                    <p>{about.description1}</p>
+                    <br />
+                    <p>{about.description2}</p>
+                </div>
+                <div className="about-graphic-container">
+                    <img className="about-graphic" src={logo} alt="logo" />
+                </div>
+            </div>
+            <div className="about-events">
+
+            </div >
+            <div className="button-smaller-container">
+                <a className="button-smaller" href={about.scheduleLink}>
+                    {about.scheduleCTA}
+                </a>
+            </div>
+            
         </div>
     </SectionWrapper>
 );
