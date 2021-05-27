7c0a9789acfb0b164e4532f073c6eb38745ae568
Amy Shao <amyshao15@gmail.com>
2021-05-27 00:47:24 -0400

    24: about page mobile and graphics added (#25)

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -3,18 +3,17 @@ import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
 import "./About.scss";
 import about from "../../resources/strings/about";
 import logo from "../../resources/images/logo/logo.png";
+import prepareTile from "../../resources/images/graphics/prepare-tile.svg";
+import hackTile from "../../resources/images/graphics/hack-tile.svg";
+import connectTile from "../../resources/images/graphics/connect-tile.svg";
 
 const About = () => (
     <SectionWrapper id="about" >
         <div className="about-container">
             <div className="about-content">
                 <div className="about-text">
-                    <h1>
-                        {about.title}
-                    </h1>
-                    <br />
+                    <h1>{about.title}</h1>
                     <p>{about.description1}</p>
-                    <br />
                     <p>{about.description2}</p>
                 </div>
                 <div className="about-graphic-container">
@@ -22,16 +21,38 @@ const About = () => (
                 </div>
             </div>
             <div className="about-events">
-
+                <div style={{ gridArea: "prep-title" }}>
+                    <h3>{about.prepareTitle}</h3>
+                    <p>{about.prepareDates}</p>
+                </div>
+                <img className="prep-image" style={{ gridArea: "prep-image" }} src={prepareTile} alt="prepare-tile" />
+                <div style={{ gridArea: "prep-caption"}} className="about-caption">
+                    <p>{about.prepareCaption}</p>
+                </div>
+                <div style={{ gridArea: "hack-title" }}>
+                    <h3>{about.hackTitle}</h3>
+                    <p>{about.hackDates}</p>
+                </div>
+                <img className="hack-image" style={{ gridArea: "hack-image" }} src={hackTile} alt="hack-tile" />
+                <div style={{gridArea: "hack-caption"}} className="about-caption">
+                    <p>{about.hackCaption}</p>
+                </div>
+                <div style={{gridArea: "connect-title"}}>
+                    <h3>{about.connectTitle}</h3>
+                    <p>{about.connectDates}</p>
+                </div>
+                <img className="connect-image" style={{gridArea: "connect-image"}} src={connectTile} alt="connect-tile" />
+                <div style={{gridArea: "connect-caption"}} className="about-caption">
+                    <p>{about.connectCaption}</p>
+                </div>
             </div >
             <div className="button-smaller-container">
                 <a className="button-smaller" href={about.scheduleLink}>
                     {about.scheduleCTA}
                 </a>
             </div>
-            
         </div>
-    </SectionWrapper>
+    </SectionWrapper >
 );
 
 export default About;
