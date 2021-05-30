752d4cd8af8c4440575e914defe9cd11f04f405c
Amy Shao <amyshao15@gmail.com>
2021-05-29 22:17:17 -0400

    sponsor-us-graphics-mobile

--- src/sections/Sponsor/SponsorUs.jsx
+++ src/sections/Sponsor/SponsorUs.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 //import Carousel from 'react-bootstrap/Carousel';
 import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
+import bee from "../../resources/images/graphics/bee.svg";
+import leaf from "../../resources/images/graphics/leaf.svg";
 import "./SponsorUs.scss";
 
 const SponsorUs = () => (
     <SectionWrapper id="sponsor-us">
         <div className="sponsor-us-container">
-            <h2 className="sponsor-us-title">Sponsor Us</h2>
-            <div className="sponsor-us-description">
-                <p>Support more than 200 young women in the next steps
+            <div className="sponsor-us-content">
+                <div className="sponsor-us-text">
+                    <h1 className="sponsor-us-title">Sponsor Us</h1>
+                    <div className="sponsor-us-description">
+                        <p>Support more than 200 young women in the next steps
                 of their journey by sponsoring the next generation of leaders</p>
-                <p>Interested in sponsoring? Email us at {'\u00A0'}
-                    <a href="mailto:sponsors@itstechnova.org">sponsors@itstechnova.org</a>
-                </p>
+                        <p>Interested in sponsoring? Email us at{" "}
+                            <a href="mailto:sponsors@itstechnova.org">sponsors@itstechnova.org</a>
+                        </p>
+                    </div>
+                    <button className="button-smaller">Join as a Sponsor</button>
+                </div>
+                <div className="sponsor-us-images">
+                    <img className="leaves" src={leaf} alt="leaves" />
+                    <img className="bee" src={bee} alt="bee" />
+                </div>
             </div>
-            <div className="sponsor-us-image"> </div> {/*TBD by design*/}
-            <button className="sponsor-us-button">Join as a Sponsor</button>
-            <div className="sponsor-us-showcase">Sponsor Showcase</div>
-
+            <h2 className="sponsor-us-showcase">Sponsor Showcase</h2>
         </div>
     </SectionWrapper>
 );
