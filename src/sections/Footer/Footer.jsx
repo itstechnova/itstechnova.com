30d9ce585d45d0d6d297b71c3b0cd4d9ea21e47d
Amy Shao <amyshao15@gmail.com>
2021-05-30 03:35:49 -0400

    Feature/38 sponsors logos page (#38)
    * sponsor-us-graphics-mobile
    
    * 38: logos mobile without links
    
    * add footer and minor sponsor changes
    
    * minor changes everywhere
    
    * minor changes everywhere again

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Footer.scss";
-
+import logo from "../../resources/images/logo/apple-touch-icon.png"
 const Footer = () => (
     <React.Fragment id="footer">
-        <div className="Footer-container">
-            <span className="title">Footer</span>
+        <div className="footer-container">
+            <div className="footer-logo-container">
+                <img src={logo} alt="logo" />
+                <span className="footer-title">TechNova</span>
+            </div>
+            <p className="title">Made with ❤️ by UWaterloo Students</p>
         </div>
     </React.Fragment>
 );
