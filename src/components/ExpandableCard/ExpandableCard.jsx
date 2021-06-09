1606b2a84b4bd98ac62a06e86ffdc66c0f4e687c
Bonnie Peng <64988958+bonniepeng2002@users.noreply.github.com>
2021-06-09 18:35:42 -0300

    animated faq! (#48)

--- src/components/ExpandableCard/ExpandableCard.jsx
+++ src/components/ExpandableCard/ExpandableCard.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import expand from "../../resources/images/icons/expand_card.svg";
 import "./ExpandableCard.scss";
 
 const ExpandableCard = (props) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <details className="card">
-      <summary>
+    <button onClick={() => setExpanded(!expanded)} className="card">
+      <div className="summary">
         <h1 className="question">{props.question}</h1>
-        <img src={expand} className="faq-arrow" alt="expand card"/>
-      </summary>
-      <div className="answer">{props.children}</div>
-    </details>
+        {/* eslint-disable-next-line jsx-a11y/role-supports-aria-props */}
+        <img
+          src={expand}
+          alt="expand card"
+          className="faq-arrow"
+          aria-expanded={expanded}
+        />
+      </div>
+      <div className="answer-container" aria-expanded={!expanded}>
+        <div className="answer">{props.children}</div>
+      </div>
+    </button>
   );
 };
 
