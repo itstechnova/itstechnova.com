b2e018cb4e77711fc922cb3bc1229719bf3f414c
gracenng <42276283+gracenng@users.noreply.github.com>
2021-06-20 15:11:10 -0400

    Add Expo + resize

--- src/sections/Sponsor/OurSponsors.jsx
+++ src/sections/Sponsor/OurSponsors.jsx
@@ -10,7 +10,8 @@ import amazon from "../../resources/images/sponsors/amazon.png";
 import hrt from "../../resources/images/sponsors/hrt.png";
 import zynga from "../../resources/images/sponsors/zynga.png";
 import cockroach from "../../resources/images/sponsors/cockroach.png";
-//import expo from "../../resources/images/sponsors/expo.png";
+import expo from "../../resources/images/sponsors/expo.png";
+import sps from "../../resources/images/sponsors/sps.png";
 
 /*
 import facebook from "../../resources/images/sponsors/bronze/facebook.png";
@@ -18,22 +19,28 @@ import splunk from "../../resources/images/sponsors/bronze/splunk.png";
 */
 import wie from "../../resources/images/logo/wie.png";
 import wics from "../../resources/images/logo/wics.png";
-
+const amazonStyle = {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    alignContent: "center",
+    width: "60%",
+};
 const OurSponsors = () => (
     <div className="our-sponsors-container">
         <h2 className="our-sponsors-title">Our Sponsors</h2>
         <div className="our-sponsors-logos">
             <div className="our-sponsors-gold">
                 <a href="https://www.aboutamazon.com/">
-                    <img src={amazon} alt="Amazon" />
+                    <img src={amazon} alt="Amazon" style={amazonStyle}/> 
                 </a>
                 <a href="https://www.cockroachlabs.com/careers/">
                     <img src={cockroach} alt="Cockroach Labs" />
                 </a>
+                {/* <a href="https://www.spscommerce.com/">
+                    <img src={sps} alt="SPS Commerce" />
+                </a> */}
                 {/*
-                    <a href="https://www.cockroachlabs.com/careers/">
-                        <img src={expo} alt="Expo" />
-                    </a>
                     <a href="https://about.google/">
                         <img src={google} alt="Google" />
                     </a>
@@ -50,7 +57,7 @@ const OurSponsors = () => (
                     <img src={scotiabank} alt="Scotiabank" />
                 </a>
                 <a href="https://www.pagerduty.com/careers/">
-                    <img src={pagerduty} alt="PagerDuty" />
+                    <img src={pagerduty} alt="PagerDuty" style={{width: "70%"}}/>
                 </a>
                 {/*
                     <a href="https://about.google/">
@@ -64,10 +71,13 @@ const OurSponsors = () => (
             </div>
             <div className="our-sponsors-bronze">
                 <a href="https://www.hudsonrivertrading.com/careers/">
-                    <img src={hrt} alt="hrt" />
+                    <img src={hrt} alt="hrt" style={{width:"170%"}}/>
                 </a>
                 <a href="https://www.zynga.com/jobs/careers/">
                     <img src={zynga} alt="zynga" />
+                </a>
+                <a href="https://www.cockroachlabs.com/careers/">
+                        <img src={expo} alt="Expo" />
                 </a>
                 {/*
                     <a href="https://about.facebook.com/">
