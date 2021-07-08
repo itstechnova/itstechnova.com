79445386b5936ff628fcf1702cbe787ffc79897c
gracenng <42276283+gracenng@users.noreply.github.com>
2021-07-08 15:28:41 -0400

    roger, fb, resizes

--- src/sections/Sponsor/OurSponsors.jsx
+++ src/sections/Sponsor/OurSponsors.jsx
@@ -4,6 +4,7 @@ import "./OurSponsors.scss";
 import google from "../../resources/images/sponsors/silver/google.png";
 import wish from "../../resources/images/sponsors/silver/wish.png";
 */
+import facebook from "../../resources/images/sponsors/facebook.png";
 import scotiabank from "../../resources/images/sponsors/scotiabank.png";
 import pagerduty from "../../resources/images/sponsors/pagerduty.png";
 import amazon from "../../resources/images/sponsors/amazon.png";
@@ -13,9 +14,8 @@ import cockroach from "../../resources/images/sponsors/cockroach.png";
 import expo from "../../resources/images/sponsors/expo.png";
 import ratiocity from "../../resources/images/sponsors/ratiocity.png";
 import sps from "../../resources/images/sponsors/sps.png";
-
+import rogers from "../../resources/images/sponsors/rogers.png";
 /*
-import facebook from "../../resources/images/sponsors/bronze/facebook.png";
 import splunk from "../../resources/images/sponsors/bronze/splunk.png";
 */
 import wie from "../../resources/images/logo/wie.png";
@@ -35,9 +35,6 @@ const OurSponsors = () => (
                 <a href="https://jobs.spscommerce.com/">
                     <img src={sps} alt="SPS Commerce" />
                 </a>
-                {/* <a href="https://www.spscommerce.com/">
-                    <img src={sps} alt="SPS Commerce" />
-                </a> */}
                 {/*
                     <a href="https://about.google/">
                         <img src={google} alt="Google" />
@@ -51,11 +48,14 @@ const OurSponsors = () => (
                     */}
             </div>
             <div className="our-sponsors-silver">
+                <a href="https://about.facebook.com/">
+                        <img src={facebook} alt="facebook" style={{width: "130%"}}/>
+                </a>
                 <a href="https://www.scotiabank.com/ca/en/about.html">
-                    <img src={scotiabank} alt="Scotiabank" />
+                    <img src={scotiabank} alt="Scotiabank" style={{width: "120%"}}/>
                 </a>
                 <a href="https://www.pagerduty.com/careers/">
-                    <img src={pagerduty} alt="PagerDuty" style={{width: "80%"}}/>
+                    <img src={pagerduty} alt="PagerDuty" style={{width: "90%"}}/>
                 </a>
                 {/*
                     <a href="https://about.google/">
@@ -69,26 +69,20 @@ const OurSponsors = () => (
             </div>
             <div className="our-sponsors-bronze">
                 <a href="https://expo.io/">
-                        <img src={expo} alt="Expo" />
+                        <img src={expo} alt="Expo" style={{width: "80%"}}/>
                 </a>
                 <a href="https://www.hudsonrivertrading.com/campus-recruiting/">
                     <img src={hrt} alt="hrt" style={{width: "80%"}} />
                 </a>
-                <a href="https://www.ratio.city/careers">
-                        <img src={ratiocity} alt="Ratio.city" style={{width: "70%"}} />
+                <a href="https://www.ratio.city/">
+                        <img src={ratiocity} alt="Ratio.city" style={{width: "50%"}} />
+                </a>
+                <a href="https://campus.rogers.com/">
+                        <img src={rogers} alt="Rogers" />
                 </a>
                 <a href="https://www.zynga.com/jobs/careers/">
                     <img src={zynga} alt="zynga" />
                 </a>
-                
-                {/*
-                    <a href="https://about.facebook.com/">
-                        <img src={facebook} alt="facebook" />
-                    </a>
-                    <a href="https://www.splunk.com/">
-                        <img src={splunk} alt="splunk" />
-                    </a>
-                    */}
             </div>
         </div>
         <h2 className="our-sponsors-title">Our Partners</h2>
