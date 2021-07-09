3708c1f55a5a21b78b68adfc808454bd3344cf96
Amy Shao <amyshao15@gmail.com>
2021-07-08 23:44:43 -0400

    updated to 2022 mlh banner

--- src/sections/Landing/Landing.jsx
+++ src/sections/Landing/Landing.jsx
@@ -44,15 +44,21 @@ const Landing = () => {
                         <span className="landing-title">{landing.title1}</span>
                         {landing.title2}
                     </h4>
-                    <h4 className="landing-logo-text-mobile">
-                        <span className="landing-title">{landing.title1}</span>
+                    <h4 className="landing-logo-text-mobile" style={{alignItems: "flex-end", justifyContent: "center", gap: "5px"}} >
+                        <img 
+                            className="mlh-mobile"
+                            style={{height: "18px", marginRight: "2px", marginBottom: "4px"}} 
+                            alt="mlh logo"
+                        />
+                        • <span className="landing-title" style={{marginBottom: "1px"}}>{landing.title1}</span>
                         {landing.title2}
                     </h4>
                     <TextLoop />
-                    <h4 className="landing-description">
-                        {landing.description}</h4>
-                    <h4 className="landing-dates">{landing.dates}</h4>
                     <h4 className="landing-description-mobile">
+                        {landing.description}
+                    </h4>
+                    <h4 className="landing-dates">{landing.dates}</h4>
+                    <h4 className="landing-description">
                         {landing.description}
                     </h4>
                 </div>
--- src/sections/NavBar/NavBar.jsx
+++ src/sections/NavBar/NavBar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { HashLink as Link } from "react-router-hash-link";
 import "./NavBar.scss";
 import ModeToggle from "../../components/ModeToggle/ModeToggle";
-import mlhBanner from "../../resources/images/graphics/mlh-banner.svg";
 import cross from "../../resources/images/icons/cross.svg";
 import hamburger from "../../resources/images/icons/hamburger.svg";
 
@@ -54,7 +53,10 @@ const NavBar = ({ routes, logoRoute }) => {
                         </div>
                     </div>
                 </div>
-                {<img className="mlh-banner" src={mlhBanner} alt="mlh-banner" />}
+                <a className="mlh-banner" href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white" rel="noreferrer" target="_blank">
+                    <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-white.svg" alt="Major League Hacking 20" />
+                </a>
+
                 {showNav && <div className="nav-mobile-links">
                     {routes.map((route) => (
                         <Link
@@ -70,6 +72,16 @@ const NavBar = ({ routes, logoRoute }) => {
                             {route.label}
                         </Link>
                     ))}
+                    <div style={{ display: "flex", justifyContent: "center", width: "100%", marginTop: "20px"}}>
+                        <a
+                            href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white"
+                            rel="noreferrer" target="_blank"
+                            style={{ textDecoration: "none", fontWeight: "normal", display: "flex", alignItems:"center", gap: "5px"}}
+                        >
+                            <img className="mlh-mobile" src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-black.svg" style={{ height: "18px", marginRight: "2px", marginBottom: "4px" }} alt="mlh logo" />
+                            official member
+                        </a>
+                    </div>
                 </div>
                 }
             </div>
